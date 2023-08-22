55c5cb2c3d4c3c7434fac1e625c634fc71dd648f
Blaine Kennedy <blainekennedy@gmail.com>
2023-08-22 15:32:51 -0500

    refactor turn_spec

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -3,44 +3,43 @@ require './lib/card'
 require 'pry'
 
 RSpec.describe Turn do
-
+ card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
   it 'exists' do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
 
     expect(turn).to be_instance_of(Turn)
   end
+    it 'is a card' do
+    turn = Turn.new("Juneau", card)
+
+    expect(turn.card).to eq(card)
+  end
 
   it 'has a guess' do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
 
     expect(turn.guess).to eq("Juneau")
   end
 
-  it 'has a guess' do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+  it 'can be correct?' do
     turn = Turn.new("Juneau", card)
 
     expect(turn.correct?).to be true
    end 
 
-   it 'has a guess' do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+   it 'can be correct?' do
     turn = Turn.new("OKC", card)
 
     expect(turn.correct?).to be false 
    end  
 
-   it 'has a guess' do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+   it 'guess is correct' do
     turn = Turn.new("Juneau", card)
 
     expect(turn.feedback).to eq("Correct!")
    end 
    
-   it 'has a guess' do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+   it 'guess is incorrect' do
     turn = Turn.new("OKC", card)
 
     expect(turn.feedback).to eq("Incorrect.")
