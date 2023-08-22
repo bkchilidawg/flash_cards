5486e6765628e3ad7d98d1ecc21de426441bb570
Blaine Kennedy <blainekennedy@gmail.com>
2023-08-22 16:48:47 -0500

    add .cards menthod

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,4 +1,11 @@
 class Deck 
-    
+    attr_reader :cards,:count
+    def initialize(cards)
+        @cards = cards
+        @count = count.length 
+    end
+    def cards_in_catagory()
+
+    end
 
 end
